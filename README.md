03762c9353dbe490c120325aaf0063350308fb08
chitpaingsoe <chitpaingsoe2013.myanmar@gmail.com>
2020-09-22 15:56:08 +0630

    update ReadMe.md

--- README.md
+++ README.md
@@ -13,18 +13,65 @@ npm install --save bodyfix-view
 ## Usage
 
 ```jsx
-import React, { Component } from 'react'
+import React, { useState } from 'react';
+import { BodyFixView } from 'bodyfix-view';
+import 'bodyfix-view/dist/index.css';
 
-import MyComponent from 'bodyfix-view'
-import 'bodyfix-view/dist/index.css'
+//default options
+const default_options = {
+	loadModelOptions: {
+		architecture: 'MobileNetV1',
+		outputStride: 16,
+		multiplier: 0.75,
+		quantBytes: 2,
+		algorithm: "person"
+	},
+	mediaOptions: {
+		video: { width: 640, height: 480 }, audio: true
+	},
+	bodyPixOptions: {
+		flipHorizontal: false,
+		internalResolution: 'medium',
+		segmentationThreshold: 0.7,
+		maxDetections: 1,
+		scoreThreshold: 0.5,
+		nmsRadius: 20,
+		estimate: 'segmentation',
+		algorithm: "person"
+	},
 
-class Example extends Component {
-  render() {
-    return <MyComponent />
-  }
 }
+
+const App = () => {
+  let options = {
+      //your custom options
+  }
+  const [start, setStart] = useState(false);
+  const [visible, setVisible] = useState(false);
+  const [mask, setMask] = useState('person');
+  const onEvent = (event) => {
+    console.log(event)
+  }
+  return <div style={{ margin: "20px" }}>
+    <button style={{ marginRight: "10px" }} onClick={() => { setStart(!start) }}>{start ? "Stop" : "Start"}</button>
+    <button style={{ marginRight: "10px" }} onClick={() => { setMask("person") }}> Mask (Person)</button>
+    <button style={{ marginRight: "10px" }} onClick={() => { setMask("room") }}>Mask (Room)</button>
+    <button style={{ marginRight: "10px" }} onClick={() => { setMask("none") }}>Mask (None)</button>
+    <button style={{ marginRight: "10px" }} onClick={() => { setVisible(!visible) }}>{visible ? "Hide View" : "Show View"}</button>
+    <br /> <br />
+    <BodyFixView
+      start={start}
+      options={options}
+      onEvent={onEvent}
+      maskType={mask}
+      visible={visible}
+    />
+  </div>
+}
+
+export default App
 ```
 
 ## License
 
-MIT © [Chit Paing Soe](https://github.com/Chit Paing Soe)
+MIT © [Chit Paing Soe](https://github.com/chitpaingsoe)
--- example/src/App.js
+++ example/src/App.js
@@ -1,35 +1,56 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { BodyFixView } from 'bodyfix-view';
+import 'bodyfix-view/dist/index.css';
 
-import { BodyFixView } from 'bodyfix-view'
-import 'bodyfix-view/dist/index.css'
+//options
+const default_options = {
+	loadModelOptions: {
+		architecture: 'MobileNetV1',
+		outputStride: 16,
+		multiplier: 0.75,
+		quantBytes: 2,
+		algorithm: "person"
+	},
+	mediaOptions: {
+		video: { width: 640, height: 480 }, audio: true
+	},
+	bodyPixOptions: {
+		flipHorizontal: false,
+		internalResolution: 'medium',
+		segmentationThreshold: 0.7,
+		maxDetections: 1,
+		scoreThreshold: 0.5,
+		nmsRadius: 20,
+		estimate: 'segmentation',
+		algorithm: "person"
+	},
 
-
-
+}
 
 const App = () => {
-  let options ={   
-  
+  let options = {
+      //your custom options
   }
-  const [start,setStart] = useState(false);
-  const [visible,setVisible] = useState(false);
-  const [mask,setMask] = useState('person');
-  const onEvent=(event)=>{
+  const [start, setStart] = useState(false);
+  const [visible, setVisible] = useState(false);
+  const [mask, setMask] = useState('person');
+  const onEvent = (event) => {
     console.log(event)
   }
-  return <div style={{margin:"20px"}}>
-    <button  style={{marginRight: "10px"}}  onClick={()=>{ setStart(!start)}}>{start ? "Stop" : "Start"}</button>
-    <button style={{marginRight: "10px"}} onClick={()=>{ setMask("person")}}> Mask (Person)</button>
-    <button style={{marginRight: "10px"}} onClick={()=>{ setMask("room")}}>Mask (Room)</button>
-    <button style={{marginRight: "10px"}} onClick={()=>{ setMask("none")}}>Mask (None)</button>
-    <button style={{marginRight: "10px"}} onClick={()=>{ setVisible(!visible)}}>{visible ? "Hide View" : "Show View"}</button>
+  return <div style={{ margin: "20px" }}>
+    <button style={{ marginRight: "10px" }} onClick={() => { setStart(!start) }}>{start ? "Stop" : "Start"}</button>
+    <button style={{ marginRight: "10px" }} onClick={() => { setMask("person") }}> Mask (Person)</button>
+    <button style={{ marginRight: "10px" }} onClick={() => { setMask("room") }}>Mask (Room)</button>
+    <button style={{ marginRight: "10px" }} onClick={() => { setMask("none") }}>Mask (None)</button>
+    <button style={{ marginRight: "10px" }} onClick={() => { setVisible(!visible) }}>{visible ? "Hide View" : "Show View"}</button>
     <br /> <br />
-    <BodyFixView 
-            start={start}
-            options={options}
-            onEvent={onEvent}
-            maskType={mask}
-            visible={visible}
-  />
+    <BodyFixView
+      start={start}
+      options={options}
+      onEvent={onEvent}
+      maskType={mask}
+      visible={visible}
+    />
   </div>
 }
 
